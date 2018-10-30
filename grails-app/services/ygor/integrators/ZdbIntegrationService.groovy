0eac409723fbd64280e470acea892310c8dc4eaf
Philipp von Böselager <philipp.boeselager@web.de>
2018-10-30 14:22:05 +0100

    Refactor ZDB data integration

--- grails-app/services/ygor/integrators/ZdbIntegrationService.groovy
+++ grails-app/services/ygor/integrators/ZdbIntegrationService.groovy
@@ -3,69 +3,70 @@ package ygor.integrators
 import de.hbznrw.ygor.connectors.KbartConnector
 import de.hbznrw.ygor.export.DataContainer
 import de.hbznrw.ygor.processing.MultipleProcessingThread
-import de.hbznrw.ygor.readers.KbartReaderConfiguration
 import de.hbznrw.ygor.readers.ZdbReader
 import grails.transaction.Transactional
-import org.slf4j.Logger
 import ygor.Record
-import ygor.field.FieldKeyMapping
 import ygor.field.MappingsContainer
-import ygor.field.MultiField
-import ygor.identifier.AbstractIdentifier
-import ygor.identifier.ZdbIdentifier
 
 @Transactional
 class ZdbIntegrationService {
 
     def integrate(MultipleProcessingThread owner, DataContainer dataContainer,
                          MappingsContainer mappingsContainer) {
+
+        Set<String> observedZdbIds = new HashSet<>()
+        Set<String> observedEissns = new HashSet<>()
+        Set<String> observedPissns = new HashSet<>()
+
         // owner.setProgressTotal(1) TODO: value?
         ZdbReader reader = owner.zdbReader
 
-        for (Record item in dataContainer.recordsPerZdbId) {
-            for (String key in owner.KEY_ORDER) {
-                if (key == KbartConnector.KBART_HEADER_ZDB_ID){
-
+        Map<String, String> readData = null
+        for (String key in owner.KEY_ORDER) {
+            if (key == KbartConnector.KBART_HEADER_ZDB_ID){
+                for (Record item in dataContainer.recordsPerZdbId) {
+                    if (item.zdbId && !observedZdbIds.contains(item.zdbId)){
+                        readData = reader.readItemData(owner.zdbKeyMapping, item.zdbId)
+                        integrateWithExisting(item, readData)
+                        addToObserved(item, observedZdbIds, observedEissns, observedPissns)
+                    }
                 }
             }
-        }
-
-        for (ZdbIdentifier zdbId : dataContainer.recordsPerZdbId.keySet()){
-
+            else if (key == KbartConnector.KBART_HEADER_ONLINE_IDENTIFIER){
+                for (Record item in dataContainer.recordsPerEissn) {
+                    if (item.eissn && !observedEissns.contains(item.eissn)){
+                        readData = reader.readItemData(owner.eissnKeyMapping, item.eissn)
+                        integrateWithExisting(item, readData)
+                        addToObserved(item, observedZdbIds, observedEissns, observedPissns)
+                    }
+                }
+            }
+            else if (key == KbartConnector.KBART_HEADER_PRINT_IDENTIFIER){
+                for (Record item in dataContainer.recordsPerPissn) {
+                    if (item.pissn && !observedPissns.contains(item.pissn)){
+                        readData = reader.readItemData(owner.pissnKeyMapping, item.pissn)
+                        integrateWithExisting(item, readData)
+                        addToObserved(item, observedZdbIds, observedEissns, observedPissns)
+                    }
+                }
+            }
         }
     }
 
-    def integrateKbart(MultipleProcessingThread owner, DataContainer data,
-                  MappingsContainer container, KbartReaderConfiguration kbartReaderConfiguration) {
+    private void integrateWithExisting(Record item, Map<String, String> readData){
+        // TODO
+    }
 
 
-        List<FieldKeyMapping> idMappings = [owner.zdbKeyMapping, owner.pissnKeyMapping, owner.eissnKeyMapping]
-        List<AbstractIdentifier> identifiers
-
-        // JsonOutput items = reader.readItems()
-        // items.each { item ->
-        Map<String, String> item = reader.readItemData(null, null)
-        while (item != null) {
-            // collect all identifiers (zdb_id, online_identifier, print_identifier) from the record
-
-            // fill record with all non-identifier fields
-            item.each { key, value ->
-                def fieldKeyMapping = container.getMapping(key, MappingsContainer.KBART)
-                if (fieldKeyMapping == null) {
-                    fieldKeyMapping = new FieldKeyMapping(false,
-                            [(MappingsContainer.YGOR) : value,
-                             (MappingsContainer.KBART): value,
-                             (MappingsContainer.ZDB)  : "",
-                             (MappingsContainer.EZB)  : ""])
-                }
-                if (null == owner.identifierByKey[fieldKeyMapping]) {
-                    MultiField multiField = new MultiField(fieldKeyMapping)
-                    multiField.addValue(SOURCE, value)
-                    record.addMultiField(multiField)
-                }
-            }
-            data.putRecord(record)
-            item = reader.readItemData(null, null)
+    private void addToObserved(Record item, def observedZdbIds, def observedEissns, def observedPissns){
+        if (item.zdbId){
+            observedZdbIds.add(item.zdbId)
+        }
+        if (item.eissn){
+            observedEissns.add(item.eissn)
+        }
+        if (item.pissn){
+            observedPissns.add(item.pissn)
         }
     }
 }
