11cee5c4646992043112ee5b01bc49868288561b
Philipp von Böselager <philipp.boeselager@web.de>
2019-07-10 15:23:12 +0200

    Code cleanup

--- src/groovy/de/hbznrw/ygor/bridges/EzbBridge.groovy
+++ src/groovy/de/hbznrw/ygor/bridges/EzbBridge.groovy
@@ -1,11 +1,9 @@
 package de.hbznrw.ygor.bridges
 
-import de.hbznrw.ygor.processing.MultipleProcessingThread
 import groovy.util.logging.Log4j
 import de.hbznrw.ygor.connectors.*
 import de.hbznrw.ygor.enums.Query
 import de.hbznrw.ygor.interfaces.*
-import de.hbznrw.ygor.export.structure.TitleStruct
 
 @Log4j
 class EzbBridge extends AbstractBridge implements BridgeInterface {
@@ -26,37 +24,13 @@ class EzbBridge extends AbstractBridge implements BridgeInterface {
 	@Override
 	void go() throws Exception {
 		log.info("Input:  " + options.get('inputFile'))
-        
         master.enrichment.dataContainer.info.api << connector.getAPIQuery('<zdbid>', null)
-        
         processor.setBridge(this)
         processor.processFile(options)
 	}
-	
+
 	@Override
 	void go(String outputFile) throws Exception {
 		log.warn("deprecated function call go(outputFile)")
 	}
-    
-    @Override
-    void processStash() throws Exception {
-        log.info("processStash()")
-        
-        def stash = processor.getStash()
-
-        MultipleProcessingThread.KEY_ORDER.each { keyType ->
-            stash.get(keyType).each { uid, key ->
-
-                if (!master.isRunning) {
-                    log.info('Aborted by user action.')
-                    return
-                }
-
-                increaseProgress()
-                connector.poll(key, stash.getKeyType(uid), null)
-
-                processor.processEntry(master.enrichment.dataContainer, uid, key)
-            }
-        }
-    }
 }
