ca211298e60f99d7da8bdb398298509013de2fb8
Philipp von Böselager <philipp.boeselager@web.de>
2019-12-02 11:12:54 +0100

    Add property 'index' to Field for multiple field entris of same source

--- grails-app/domain/ygor/field/Field.groovy
+++ grails-app/domain/ygor/field/Field.groovy
@@ -7,6 +7,7 @@ class Field {
 
   String source
   String key
+  int index
   String value
 
   static constraints = {
@@ -15,20 +16,31 @@ class Field {
     value nullable: false
   }
 
+
   Field(String source, String key, String value) {
     this.source = source
     this.key = key
+    this.index = 0 // default index is 0 because for most fields we expect only 1 entry
     this.value = value
   }
+
+
+  Field(String source, String key, int index, String value) {
+    this(source, key, value)
+    this.index = index
+  }
+
 
   String toString() {
     "Field: ".concat(source).concat("-").concat(key).concat(": ").concat(value)
   }
 
+
   void asJson(JsonGenerator jsonGenerator) {
     jsonGenerator.writeStartObject()
     jsonGenerator.writeStringField("source", source)
     jsonGenerator.writeStringField("key", key)
+    jsonGenerator.writeNumberField("index", index)
     jsonGenerator.writeStringField("value", value)
     jsonGenerator.writeEndObject()
   }
