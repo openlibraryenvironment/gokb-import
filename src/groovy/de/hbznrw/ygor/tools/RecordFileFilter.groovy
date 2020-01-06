d1ebb563343fca40db92c3102cd9f0e4e1bf730e
Philipp von Böselager <philipp.boeselager@web.de>
2020-01-06 16:12:43 +0100

    Split json structure into separate files for each record (#138)

--- src/groovy/de/hbznrw/ygor/tools/RecordFileFilter.groovy
+++ src/groovy/de/hbznrw/ygor/tools/RecordFileFilter.groovy
@@ -1,4 +1,16 @@
 package de.hbznrw.ygor.tools
 
-class RecordFileFilter{
+import java.util.regex.Pattern
+
+class RecordFileFilter implements java.io.FileFilter {
+
+  Pattern pattern
+
+  RecordFileFilter(String resultHash) {
+    pattern = java.util.regex.Pattern.compile("^".concat(resultHash).concat("_"))
+  }
+
+  boolean accept(File file) {
+    return pattern.matcher(file.getName()).find()
+  }
 }
