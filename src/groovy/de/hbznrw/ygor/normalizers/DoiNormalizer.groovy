3c72520ce0a96fd87e81e5173df9b7a31ff81492
Philipp von Böselager <philipp.boeselager@web.de>
2020-01-29 13:47:37 +0100

    Remove URL-prependix in DOI normalization (fixes #153)

--- src/groovy/de/hbznrw/ygor/normalizers/DoiNormalizer.groovy
+++ src/groovy/de/hbznrw/ygor/normalizers/DoiNormalizer.groovy
@@ -3,10 +3,6 @@ package de.hbznrw.ygor.normalizers
 class DoiNormalizer {
 
   static String normalizeDoi(String orgValue) {
-    String result = CommonNormalizer.removeSpaces(orgValue)
-    if (result.startsWith("10.")) {
-      result = "https://doi.org/".concat(result)
-    }
-    result
+    CommonNormalizer.removeSpaces(orgValue)
   }
 }
