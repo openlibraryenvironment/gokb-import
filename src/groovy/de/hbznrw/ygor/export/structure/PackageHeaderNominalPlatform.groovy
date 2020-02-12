c04457f969e0cfda19318b306e0c1e259ae0113a
Philipp von Böselager <philipp.boeselager@web.de>
2020-02-12 14:21:59 +0100

    Handle platform and provider to result (fixes #203)

--- src/groovy/de/hbznrw/ygor/export/structure/PackageHeaderNominalPlatform.groovy
+++ src/groovy/de/hbznrw/ygor/export/structure/PackageHeaderNominalPlatform.groovy
@@ -1,8 +1,18 @@
 package de.hbznrw.ygor.export.structure
 
+import com.fasterxml.jackson.databind.JsonNode
+import de.hbznrw.ygor.tools.JsonToolkit
+
 class PackageHeaderNominalPlatform {
 
   String name
   String url
+
+  static PackageHeaderNominalPlatform fromJson(JsonNode node, String subField) throws IOException{
+    PackageHeaderNominalPlatform result = new PackageHeaderNominalPlatform()
+    result.name = JsonToolkit.fromJson(node, subField.concat(".name"))
+    result.url = JsonToolkit.fromJson(node, subField.concat(".url"))
+    result
+  }
 
 }
