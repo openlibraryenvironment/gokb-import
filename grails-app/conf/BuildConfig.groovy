4339570d7008938bfe3ce8a4efbf806e351d74f3
Philipp von Böselager <philipp.boeselager@web.de>
2020-06-02 10:42:14 +0200

    Update Grails Tomcat plugin to 8.0.33 (using Tomcat 9)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -61,7 +61,7 @@ grails.project.dependency.resolution = {
 
   plugins {
     // plugins for the build system only
-    build ":tomcat:7.0.55.3" // or ":tomcat:8.0.22"
+    build ":tomcat:8.0.33"
 
     // plugins for the compile step
     compile ":scaffolding:2.1.2"
