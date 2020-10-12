8935d4c6b065047e83cce308e7f30f422adb25aa
Philipp von Böselager <philipp.boeselager@web.de>
2020-10-12 17:22:17 +0200

    Enable connection to KBart URL where server needs a user agent (fixes #296)

--- src/groovy/de/hbznrw/ygor/readers/KbartFromUrlReader.groovy
+++ src/groovy/de/hbznrw/ygor/readers/KbartFromUrlReader.groovy
@@ -10,10 +10,25 @@ class KbartFromUrlReader extends KbartReader{
   EnrichmentService enrichmentService = new EnrichmentService()
 
   KbartFromUrlReader(URL url, File sessionFolder) throws Exception{
-    String encoding = enrichmentService.getEncoding(url.openStream())
-    if (!("UTF-8".equals(encoding))){
-      throw new IllegalFormatException("Encoding of KBart file at ".concat(url.toExternalForm()).concat(" was ")
-          .concat(encoding).concat(" Only UTF-8 is allowed."))
+    HttpURLConnection connection
+    try {
+      connection = (HttpURLConnection) url.openConnection()
+      connection.addRequestProperty("User-Agent", "Mozilla/5.0")
+    }
+    catch (IOException e) {
+      throw new RuntimeException("URL Connection was not established.")
+    }
+    connection.connect()
+    if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+      String encoding = enrichmentService.getEncoding(connection.getInputStream(), url)
+      if (encoding in ["text/plain" /* add further encoding "neutral" content types here*/ ]){
+        throw new IllegalArgumentException("No encoding found for KBart file at ".concat(url.toExternalForm())
+            .concat(". File needs to be UTF-8 encoded."))
+      }
+      if (!("UTF-8".equals(encoding))){
+        throw new IllegalArgumentException("Encoding of KBart file at ".concat(url.toExternalForm()).concat(" was ")
+            .concat(encoding).concat(". Only UTF-8 is allowed."))
+      }
     }
     URLReader urlReader = new URLReader(url, Charset.forName("UTF-8"))
     String fileData = urlReader.getText()
